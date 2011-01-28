1f94673bc85d34704b366892da66c64e0e2b5b91
patrick brisbin <pbrisbin@gmail.com>
2011-01-28 10:41:27 -0500

    allow non-opts by Maybe types

--- lib/Dzen.hs
+++ lib/Dzen.hs
@@ -26,6 +26,7 @@ module Dzen (
 import System.IO
 import System.Posix.IO
 import System.Posix.Process (executeFile, forkProcess, createSession)
+import Data.List (intercalate)
 
 -- $usage
 --
@@ -60,18 +61,21 @@ import System.Posix.Process (executeFile, forkProcess, createSession)
 --
 
 -- | A data type to fully describe a spawnable dzen bar, take a look at
---   @\/usr\/share\/doc\/dzen2\/README@ to see what input is acceptable
+--   @\/usr\/share\/doc\/dzen2\/README@ to see what input is acceptable. 
+--   Options are wrapped in 'Just', so using 'Nothing' will not add that 
+--   option to the @dzen2@ executable. @exec@ and @addargs@ can be 
+--   empty.
 data DzenConf = DzenConf 
-    { x_position :: Int       -- ^ x position
-    , y_position :: Int       -- ^ y position
-    , width      :: Int       -- ^ width
-    , height     :: Int       -- ^ line height
-    , alignment  :: TextAlign -- ^ alignment of title window
-    , font       :: String    -- ^ font
-    , fg_color   :: String    -- ^ foreground color
-    , bg_color   :: String    -- ^ background color
-    , exec       :: String    -- ^ exec flags, ex: \"onstart=lower\"
-    , addargs    :: [String]  -- ^ additional arguments, ex: [\"-p\", \"-tw\", \"5\"]
+    { x_position :: Maybe Int       -- ^ x position
+    , y_position :: Maybe Int       -- ^ y position
+    , width      :: Maybe Int       -- ^ width
+    , height     :: Maybe Int       -- ^ line height
+    , alignment  :: Maybe TextAlign -- ^ alignment of title window
+    , font       :: Maybe String    -- ^ font
+    , fg_color   :: Maybe String    -- ^ foreground color
+    , bg_color   :: Maybe String    -- ^ background color
+    , exec       :: [String]        -- ^ exec flags, ex: [\"onstart=lower\", ...]
+    , addargs    :: [String]        -- ^ additional arguments, ex: [\"-p\", \"-tw\", \"5\"]
     }
 
 -- | A simple data type for the text alignment of the dzen bar
@@ -122,35 +126,40 @@ spawnToDzen x d = do
 
     return ()
 
--- | Build the right list of arguments for a dzen
+-- | Build the right list of arguments for a 'DzenConf'
 dzenArgs :: DzenConf -> [String]
-dzenArgs d = [ "-fn", quote $ font       d
-             , "-fg", quote $ fg_color   d
-             , "-bg", quote $ bg_color   d
-             , "-ta", show  $ alignment  d
-             , "-x" , show  $ x_position d
-             , "-y" , show  $ y_position d
-             , "-w" , show  $ width      d
-             , "-h" , show  $ height     d
-             ] ++ addexec (exec d) ++ addargs d
+dzenArgs d =  addOpt ("-fg", fmap quote $ fg_color   d)
+           ++ addOpt ("-bg", fmap quote $ bg_color   d)
+           ++ addOpt ("-ta", fmap show  $ alignment  d)
+           ++ addOpt ("-x" , fmap show  $ x_position d)
+           ++ addOpt ("-y" , fmap show  $ y_position d)
+           ++ addOpt ("-w" , fmap show  $ width      d)
+           ++ addOpt ("-h" , fmap show  $ height     d)
+           ++ addExec (exec d)
+           ++ addargs d
 
     where
         quote = ("'"++) . (++"'")
-        addexec s = if null s then [] else ["-e", quote s]
+
+        addOpt (_  , Nothing ) = []
+        addOpt (opt, Just arg) = [opt, arg]
+
+        addExec [] = []
+        addExec es = ["-e", quote $ intercalate ";" es]
 
 -- | A default dzen configuration. Similar colors to default decorations 
 --   and prompts in other modules. Added options @-p@ and @-e 
 --   \'onstart=lower\'@ are useful for dzen-as-statusbar.
 defaultDzen :: DzenConf
 defaultDzen = DzenConf
-    { x_position  = 0
-    , y_position  = 0
-    , width       = 1280
-    , height      = 17
-    , alignment   = LeftAlign
-    , font        = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
-    , fg_color    = "#FFFFFF"
-    , bg_color    = "#333333"
-    , exec        = "onstart=lower"
+    { x_position  = Nothing
+    , y_position  = Nothing
+    , width       = Nothing
+    , height      = Nothing
+    , alignment   = Nothing
+    , font        = Just "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
+    , fg_color    = Just "#FFFFFF"
+    , bg_color    = Just "#333333"
+    , exec        = ["onstart=lower"]
     , addargs     = ["-p"]
     }
