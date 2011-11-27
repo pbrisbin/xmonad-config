722ecdc2b2dbc01d70340ff0c1217bb3b6ba1424
patrick brisbin <pbrisbin@gmail.com>
2011-11-27 01:15:46 -0500

    new yeganesh usage

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -127,7 +127,7 @@ pbUrgencyConfig = urgencyConfig { suppressWhen = OnScreen }
 --   variable @$DMENU_OPTIONS@ to customize dmenu appearance, this is a 
 --   good @M-p@ replacement.
 yeganesh :: MonadIO m => m ()
-yeganesh = spawn "exe=`echo | yeganesh -- $DMENU_OPTIONS` && eval \"exec $exe\""
+yeganesh = spawn "exe=`yeganesh -x -- $DMENU_OPTIONS </dev/null` && eval \"exec $exe\""
 
 -- | Execute a command in the user-configured terminal.
 --
