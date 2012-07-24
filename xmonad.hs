49e9917370e84aa395604d2477b50c10f3461bd4
patrick brisbin <pbrisbin@gmail.com>
2012-07-24 09:23:06 -0400

    major refactor to use statusBar

--- xmonad.hs
+++ xmonad.hs
@@ -9,12 +9,9 @@
 import XMonad
 
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
-import XMonad.Layout.LayoutHints
 import XMonad.Util.EZConfig
-import XMonad.Util.Run
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.WorkspaceCompare
 
@@ -22,45 +19,41 @@ import qualified XMonad.StackSet as W
 
 main :: IO ()
 main = do
-    d <- spawnPipe "dzen2 -p -xs 1 -ta l -fn Verdana-8 -e 'onstart=lower'"
+    conf <- statusBar "dzen2 -p -xs 1 -ta l -fn Verdana-8 -e 'onstart=lower'"
+                dzenPP
+                    { ppSort   = getSortByXineramaRule
+                    , ppHidden = hideNSP
+                    , ppUrgent = ppUrgent dzenPP . hideNSP
+                    , ppTitle  = pad . dzenColor "#bbb" ""
+                    , ppLayout = const ""
+                    }
+                (\XConfig { modMask = m } -> (m, xK_b))
+                (defaultConfig
+                    { terminal   = "urxvtc"
+                    , modMask    = mod4Mask
+                    , manageHook = composeAll
+                                        [ isDialog     --> doCenterFloat
+                                        , isFullscreen --> doFullFloat
+                                        , namedScratchpadManageHook scratchpads
+                                        , manageHook defaultConfig
+                                        ]
+                    , startupHook = do
+                        spawn "conky -c ~/.xmonad/data/conky/main"
+                        spawn "conky -c ~/.xmonad/data/conky/dzen | dzen2 -p -xs 2 -ta r -fn Verdana-8 -e 'onstart=lower'"
+                    }
+                    `additionalKeysP`
+                        [ ("<XF86AudioMute>"       , spawn "ossvol -t"  )
+                        , ("<XF86AudioLowerVolume>", spawn "ossvol -d 1")
+                        , ("<XF86AudioRaiseVolume>", spawn "ossvol -i 1")
+                        , ("M1-x"                  , namedScratchpadAction scratchpads "ossxmix")
+                        , ("M1-m"                  , namedScratchpadAction scratchpads "mail"   )
+                        , ("M-p"                   , spawn "x=$(yeganesh -x -- $DMENU_OPTIONS) && exec $x")
+                        , ("M-a"                   , spawn $  "for pid in `pgrep conky`; do kill -9 $pid; done && "
+                                                           ++ "for pid in `pgrep dzen2`; do kill -9 $pid; done && "
+                                                           ++ "xmonad --recompile && xmonad --restart")
+                        ])
 
-    xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
-        { terminal   = "urxvtc"
-        , modMask    = mod4Mask
-        , layoutHook = avoidStruts . layoutHints
-                     $ layoutHook defaultConfig
-
-        , manageHook = manageHook defaultConfig
-                        <+> manageDocks
-                        <+> composeAll
-                            [ isDialog     --> doCenterFloat
-                            , isFullscreen --> doFullFloat
-                            , namedScratchpadManageHook scratchpads
-                            ]
-
-        , logHook = dynamicLogWithPP $ dzenPP
-                        { ppOutput = hPutStrLn d
-                        , ppSort   = getSortByXineramaRule
-                        , ppHidden = hideNSP
-                        , ppUrgent = ppUrgent dzenPP . hideNSP
-                        , ppTitle  = pad . dzenColor "#bbb" ""
-                        , ppLayout = const ""
-                        }
-
-        , startupHook = do
-            spawn "conky -c ~/.xmonad/data/conky/main"
-            spawn "conky -c ~/.xmonad/data/conky/dzen | dzen2 -p -xs 2 -ta r -fn Verdana-8 -e 'onstart=lower'"
-        }
-
-        `additionalKeysP`
-            [ ("<XF86AudioMute>"       , spawn "ossvol -t"  )
-            , ("<XF86AudioLowerVolume>", spawn "ossvol -d 1")
-            , ("<XF86AudioRaiseVolume>", spawn "ossvol -i 1")
-            , ("M-p"                   , yeganesh           )
-            , ("M-q"                   , restart            )
-            , ("M1-x"                  , namedScratchpadAction scratchpads "ossxmix")
-            , ("M1-m"                  , namedScratchpadAction scratchpads "mail"   )
-            ]
+    xmonad $ withUrgencyHook NoUrgencyHook $ conf
 
     where
         scratchpads :: [NamedScratchpad]
@@ -79,11 +72,3 @@ main = do
 
         hideNSP :: WorkspaceId -> String
         hideNSP ws = if ws /= "NSP" then pad ws else ""
-
-        yeganesh :: MonadIO m => m ()
-        yeganesh = spawn "x=$(yeganesh -x -- $DMENU_OPTIONS) && exec $x"
-
-        restart :: MonadIO m => m ()
-        restart = spawn $  "for pid in `pgrep conky`; do kill -9 $pid; done && "
-                        ++ "for pid in `pgrep dzen2`; do kill -9 $pid; done && "
-                        ++ "xmonad --recompile && xmonad --restart"
